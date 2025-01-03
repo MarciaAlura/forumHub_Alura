32e20c2533a15cbc9cc8d27ed4b989b7d5a01e98
MarciaAlura <mcrogick@ig.com.br>
2025-01-03 17:22:32 -0300

    Security 001

--- src/main/java/br/com/alura/controller/TopicoController.java
+++ src/main/java/br/com/alura/controller/TopicoController.java
@@ -1,6 +1,7 @@
 package br.com.alura.controller;
 
 import br.com.alura.domain.topico.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("topicos")
+@SecurityRequirement(name = "bearer-key")
 public class TopicoController {
 
     @Autowired
